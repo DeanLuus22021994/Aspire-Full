b37a3bdf727119e2fb99b81a48233c7b60f14867
DeanLuus22021994 <dean.luus2021994@gmail.com>
2025-11-29 03:40:45 +0200

    feat(gateway): initialize project structure with API and testing setup

---
+++ src/virtual/tests/business-tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace business_tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/virtual/tests/engine-tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace engine_tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
